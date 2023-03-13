3ac16adfad42b3a71e9f290decb10280ab5b6311
Jackson Kistler <kistlerj1@students.westerntc.edu>
2023-03-13 10:47:35 -0500

    models created, started main activity nav

--- app/src/main/java/com/example/golfapp/MainActivity.java
+++ app/src/main/java/com/example/golfapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.golfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.golfapp.models.Club;
 import com.example.golfapp.models.Stroke;
@@ -14,18 +17,28 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
+    Button btnStrokeList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Club c = new Club(1, "test", new Date(123,8,22));
-        Stroke s = new Stroke(1, new Date(), c, 100, "Hook");
+//        Club c = new Club(1, "test", new Date(123,8,22));
+//        Stroke s = new Stroke(1, new Date(), c, 100, "Hook");
+//
+//        Log.d(TAG, c.toString());
+//        Log.d(TAG, s.toString());
 
+        btnStrokeList = findViewById(R.id.btnStrokeList);
+        btnStrokeList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, StrokeListActivity.class);
+                startActivity(i);
+            }
+        });
 
-
-        Log.d(TAG, c.toString());
-        Log.d(TAG, s.toString());
 
     }
 }
---
+++ app/src/main/java/com/example/golfapp/StrokeListActivity.java
@@ -0,0 +1,13 @@
+package com.example.golfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StrokeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stroke_list);
+    }
+}
